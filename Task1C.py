af1a36bfd7a46f22c7124af9814f1c6dc34e53a1
Olumide Apanisile <oa320@cam.ac.uk>
2023-02-07 13:19:49 +0000

    Completed Task1C

--- Task1C.py
+++ Task1C.py
@@ -1,6 +1,30 @@
 # Olumide
+
 # Import the stations_within_radius function
-# Set unit on haversine function to km
-# Use the coordinates (52.2053, 0.1218) as centre with r = 10 in the function
-# Sort the results of the function in alphabetical order
-# Output a list of the results
+from floodsystem.geo import stations_within_radius
+
+# Import the build_station_list like in 1A
+from floodsystem.stationdata import build_station_list
+
+# Set Cambridge_coordinates to the ones provided on tasks webpage
+cambridge_coordinates = (52.2053, 0.1218)
+
+def run():
+    
+    # Build list of stations
+    stations = build_station_list()
+    
+    # Make a list of stations and distances within the radius
+    organised_radius_list = []
+    organised_radius_list = stations_within_radius (stations, cambridge_coordinates, 10)
+    
+    # Sort the results of the function in alphabetical order
+    sorted_radius_list = sorted (organised_radius_list)
+    
+    # Output a list of the results
+    print(sorted_radius_list)
+
+# Run the program in main branch...I think
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,6 +5,9 @@
 geographical data.
 
 """
+# Import required modules up here and across multiple functions
+from haversine import haversine, Unit
+
 #Task 1B: sort stations by distance - Olumide
 def stations_by_distance(stations, p):
 
@@ -15,7 +18,6 @@ def stations_by_distance(stations, p):
     for station in stations:
         #Where do we get p from?
         # Use the haversine function to calculate the distance of the station from the coordinates
-        from haversine import haversine, Unit
         distance = haversine(station.coord, p)
         # Add each element needed for Task1B into the station_distance list independently
         # Add the stations and respective distances to the list in a tuple format
@@ -23,6 +25,24 @@ def stations_by_distance(stations, p):
         
     # Return the sorted list as the output
     return station_distance
+
+# Task 1C: stations within radius - Olumide
+def stations_within_radius(stations, centre, r):
+
+    # Make a list to contain all the required station information
+    radius_list = []
+    
+    # Use the haversine function to calculate the distance between each station and centre
+    for station in stations:
+        distance = haversine(station.coord, centre, unit=Unit.KILOMETERS)
+        
+        # If that distance is less than r then add the corresponding station to the list
+        if distance < r:
+            radius_list.append((station.name))
+    
+    #The list with all the stations within the radius r        
+    return radius_list 
+
 
 
 
