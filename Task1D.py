8a6ddcbca023ee8360e1fa76935d7fe63062870f
Olumide Apanisile <oa320@cam.ac.uk>
2023-02-07 14:54:46 +0000

    Completed Task1D

--- Task1D.py
+++ Task1D.py
@@ -1,15 +1,57 @@
 # Olumide
 
 # Part 1
+
 # Import the rivers_with_station function from the geo.py file
-# Do a counter for the number of rivers with station = true or non zero
-# Print the number 
-# Make a list of the rivers that satisfy the condition above
-# Sort this list in alphabetical order 
-# Print a list of the first 10 items in the list
+from floodsystem.geo import rivers_with_station
+
+# Import the build_station_list like in 1A
+from floodsystem.stationdata import build_station_list
+
+'''def run():
+    
+    # Build list of stations
+    stations = build_station_list()
+    
+    # Make a set to contain the results of the rivers_with_station function
+    initial_river_list = []
+    initial_river_list = rivers_with_station(stations)
+    set_of_river_list = set(initial_river_list)
+   
+    # Print the number of items in the set
+    print(f'{len(set_of_river_list)} stations')
+    
+    # Sort the results of the function in alphabetical order
+    sorted_river_list = sorted (set_of_river_list)
+    
+    # Print a list of the first 10 items in the list
+    print (sorted_river_list[:10])
+ 
+# Run the program in main branch...I think
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()'''
 
 # Part 2
+
 # Import the stations_by_river function from the geo.py file
-# Make a list of the items from the dictionaries with the keys: River Aire, Cam and Thames
-# Sort the lists in alphabetical order
-# Print the sorted lists and theur corresponding rivers
+from floodsystem.geo import stations_by_river
+
+def run():
+    
+    # Build list of stations
+    stations = build_station_list()
+
+    # Make a dictionary with all the rivers and stations
+    river_dictionary_all = stations_by_river (stations)
+
+    #Extract the list of stations associated with the specified rivers and sort them alphabetically
+    river_name = str(input('Enter River name: '))
+    river_list = river_dictionary_all[river_name]
+    sorted_river_list = sorted (river_list)
+    print(sorted_river_list[:10])
+
+# Run the program in main branch...I think
+if __name__ == "__main__":
+    print("*** Task 1C: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -16,9 +16,11 @@ def stations_by_distance(stations, p):
     
     #A loop that iterates over every station from the stations list
     for station in stations:
+        
         #Where do we get p from?
         # Use the haversine function to calculate the distance of the station from the coordinates
         distance = haversine(station.coord, p)
+        
         # Add each element needed for Task1B into the station_distance list independently
         # Add the stations and respective distances to the list in a tuple format
         station_distance.append ((station.name, station.town, distance))
@@ -42,6 +44,53 @@ def stations_within_radius(stations, centre, r):
     
     #The list with all the stations within the radius r        
     return radius_list 
+
+# Task 1D: rivers with a station(s) Part 1 - Olumide
+def rivers_with_station(stations):
+    
+    # For every river in stations, check if there is a corresponsing station somehow
+    # Make an empty list to contain the names of all the rivers
+    rivers_list = []
+    
+    # Add the name of every river that corresponds to a station
+    for station in stations:
+        rivers_list.append (station.river)
+    
+    # Make a list of tuples of rivers and stations
+    return rivers_list
+
+# Task 1D: rivers with a station(s) Part 2 - Olumide
+def stations_by_river(stations):
+    
+    # Define a method for adding river_names as keys and associated stations as corresponding values
+    def add_values_in_dict(river_dictionary, key, stations_list):
+        # If the key does not already exist in dictionary, add it to the dictionary keys
+        if key not in river_dictionary:
+            river_dictionary[key] = list()
+            river_dictionary[key].append(stations_list)
+        # If the key already exists in the dictionary, add the station to the list of values associated with that key
+        elif key in river_dictionary:
+            river_dictionary[key].append(stations_list)
+        return river_dictionary
+    
+    # Initialise dictionary
+    river_dictionary = {}
+
+    # Iterate over very station and make lists of stations with the same river
+    for station in stations:
+        results_dictionary = add_values_in_dict (river_dictionary, station.river, station.name)
+    return results_dictionary
+    
+        
+    
+    
+
+
+
+
+
+
+
 
 
 
