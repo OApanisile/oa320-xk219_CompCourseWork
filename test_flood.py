25fabe03ac6866b9d96e2be7e8e5b3cd30920afd
XinzeKang <124386168+XinzeKang@users.noreply.github.com>
2023-02-27 02:01:22 +0000

    test for flood(2)

--- test_flood.py
+++ test_flood.py
@@ -1,6 +1,6 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
-
+from floodsystem.flood import stations_highest_rel_level
 
 #execute test#
 def test_stations_level_over_threshold():
@@ -10,3 +10,14 @@ def test_stations_level_over_threshold():
     update_water_levels(stations)
 
     over_tol_list = stations_level_over_threshold(stations, float(0.2))
+
+
+
+
+#execute test#
+def test_stations_highest_rel_level():
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+    stations_highest_rel_level(stations,3)
