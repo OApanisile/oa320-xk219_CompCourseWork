cf56abbca2cf4fc3ff558987064e3d97ccda9dca
XinzeKang <124386168+XinzeKang@users.noreply.github.com>
2023-02-27 11:26:25 +0000

    test

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -14,6 +14,8 @@ def plot_water_levels(station, dates, levels):
     # Add axis labels, rotate date labels and add plot title
     plt.xlabel('date')
     plt.ylabel('water level (m)')
+    plt.axhline(y=station[0].typical_range[0])
+    plt.axhline(y=station[0].typical_range[1])
     plt.xticks(rotation=45);
     plt.title(station[0].name)
 
